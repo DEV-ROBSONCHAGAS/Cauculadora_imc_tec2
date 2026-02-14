2b3ee56dd2e1296be9a8181b130b5f9c7412997e
DEV-ROBSONCHAGAS <djrobsondenny@hotmail.com>
2026-02-14 12:13:41 -0300

    Alterando valores nas funções

--- func_sugs.py
+++ func_sugs.py
@@ -2,7 +2,7 @@ def obter_sugestoes(imc):
     if imc < 18.5:
         return "Baixo peso", "Foco em musculação para ganho de massa e dieta hipercalórica."
     elif 18.5 <= imc < 25:
-        return "Peso Ideal", "Manutenção: Caminhadas, natação ou ciclismo 3.5x por semana."
+        return "Peso Ideal", "Manutenção: Caminhadas, natação ou ciclismo 3x por semana."
     elif 25 <= imc < 30:
         return "Sobrepeso", "Cardio moderado (corrida leve) e treinos de resistência."
     else:
